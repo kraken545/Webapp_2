2230c33f800021a1e7877255257550c15d9c9926
Elian Meran Rosario <1217378@student.roc-nijmegen.nl>
2026-05-11 16:21:28 +0200

    styling

--- index.php
+++ index.php
@@ -3,10 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/styles.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
    
-    <title>Document</title>
-<body>
-        
+    <title>Voyage</title>
+</head>
+    <div class="nav-bar">
+        <div class="logo">Voyage</div>
+
+    <nav>
+        <a href="./public/accommodation.php">Offer's</a>
+        <a href="./public/vacations.php">About Us</a>
+        <a href="./public/contact.php">Contact</a>
+    </nav>
+    <div>
+        <img src="./assets/img/icon_color/loop.png" alt="" width="25px">
+    </div>
+    <div>
+        My Account
+    </div>
+    </div>
+<body class="font">
+        <section>
+            <div>Makkelijk en 
+                goedkoop rijzen</div>
+            <div>Voordeligste reisbureau 
+                    van Nederland</div>
+        </section>
+        <section>
+            <div>van locatie1</div>      
+            <div>naar locatie2</div>
+            <div>vertrek</div>
+            <div>terug</div>
+            <div></div>
+            <div><img src="assets/img/icon_color/loop.png" alt="" width="25px"> Zoeken</div>
+        </section>
+        <section>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div></div>
+        </section>
+        <section>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
 </body>
+<footer>
+    <div class="nav-bar">
+    <nav> 
+        <a href="">contact</a>
+        <a href="">Privacy & Policy</a>
+        
+    </nav>
+    <div><img src="assets/img/icon_color/youtube.png" alt="" width="25px"></div>
+    <div><img src="assets/img/icon_color/social.png" alt="" width="25px"></div>
+    <div><img src="assets/img/icon_color/facebook.png" alt="" width="25px"></div>
+</div>
+</footer>
 </html>
