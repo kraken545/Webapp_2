52abe39e1239e0ebd10782dd3e7a3e470ecd3451
Elian Meran Rosario <1217378@student.roc-nijmegen.nl>
2026-05-22 11:25:11 +0200

    form/menu popup maken voor zoekbalk (om aantal personen te kiezen)

---
+++ assets/js/app.js
@@ -0,0 +1,83 @@
+
+
+let totalPeople = 1;
+const totalPeopleMin = 1;
+const totalPeopleMax = 10;
+
+// =========== open form  ==========
+function openForm() {
+	const popup = document.getElementById('myForm');
+	popup.classList.add('open');
+	setForm();
+}
+
+// =========== close form  ==========
+
+function closeForm() {
+	const popup = document.getElementById('myForm');
+	popup.classList.remove('open');
+}
+
+
+
+function increment() {
+		totalPeople++;
+		setForm();
+	}
+
+function decrement() {
+		totalPeople--;
+		setForm();
+	}
+
+
+
+function setForm() {
+	const display = document.getElementById('total-people');
+	const input = document.getElementById('total-people-input');
+	const addButton = document.getElementById('add-aantal');
+	display.textContent = totalPeople;
+	input.value = totalPeople;
+	addButton.textContent = `${totalPeople} travelers`;
+	const decrement = document.getElementById('decrement-people');
+	const increment = document.getElementById('increment-people');
+	if(decrement) decrement.disabled = totalPeople <= totalPeopleMin;
+	if(increment) increment.disabled = totalPeople >= totalPeopleMax;
+}
+function apply() {
+	// const addAantal= document.getElementById('add-aantal');
+	// addAantal.textContent = `Has añadido ${totalPeople} viajeros a tu reserva.`;
+	closeForm();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	setForm();
+});
+
+
+
+// function closeBigGroup(){
+// 	const bigGroup = document.getElementById('big-group');
+// 	bigGroup.classList.remove('open');
+// }
+
+// function openBigGroup() {
+// 	const bigGroup = document.getElementById('big-group');
+// 	bigGroup.classList.add('open');
+// 	setBigGroup();
+// }
+
+// function applyBigGroup() {
+// 	closeBigGroup();
+// }
+
+// function contactUs() {
+// 	window.location.href = './public/contact.php';
+// }
+
+// function setBigGroup() {
+// 	const bigGroupInput = (totalPeople - 1);
+// 	const bigGroupDisplay = document.getElementById('big-group-display');
+	
+// }
+
