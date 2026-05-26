854d5652eb0c48d8ef31a3b1d427864ceba001e0
Yonaki51 <yonivandon@hotmail.com>
2026-05-26 12:09:24 +0200

    fixed a spelling error and filled some tables

--- Flight_Company.sql
+++ Flight_Company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Gegenereerd op: 26 mei 2026 om 09:38
+-- Gegenereerd op: 26 mei 2026 om 10:04
 -- Serverversie: 8.4.8
 -- PHP-versie: 8.3.30
 
@@ -24,15 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `accomodations`
+-- Tabelstructuur voor tabel `accommodations`
 --
 
-CREATE TABLE `accomodations` (
-  `accomodationid` int NOT NULL,
+CREATE TABLE `accommodations` (
+  `accommodationid` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `peopleamount` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `accommodations`
+--
+
+INSERT INTO `accommodations` (`accommodationid`, `name`, `type`, `peopleamount`) VALUES
+(1, 'Hotel V Nesplein', 'Hotel', 4),
+(2, 'Hotel Le Marais', 'Hotel', 2),
+(3, 'The Manhattan Hotel', 'Hotel', 10),
+(4, 'Tokyo Guesthouse Asakusa', 'Guesthouse', 5),
+(5, 'Hotel Arts Barcelona', 'Hotel', 6),
+(6, 'The Savoy', 'Hotel', 3),
+(7, 'Sydney Harbour Lodge', 'Lodge', 8),
+(8, 'Hotel Bella Roma', 'Hotel', 4);
 
 -- --------------------------------------------------------
 
@@ -45,6 +59,19 @@ CREATE TABLE `flights` (
   `departure` varchar(255) NOT NULL,
   `destination` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `flights`
+--
+
+INSERT INTO `flights` (`flightid`, `departure`, `destination`) VALUES
+(1, 'Amsterdam', 'Parijs'),
+(2, 'Amsterdam', 'New York'),
+(3, 'Amsterdam', 'Tokyo'),
+(4, 'Amsterdam', 'Barcelona'),
+(5, 'Amsterdam', 'Londen'),
+(6, 'Amsterdam', 'Sydney'),
+(7, 'Amsterdam', 'Rome');
 
 -- --------------------------------------------------------
 
@@ -112,7 +139,7 @@ CREATE TABLE `trips` (
   `duration` int NOT NULL,
   `description` text NOT NULL,
   `flightid` int DEFAULT NULL,
-  `accomodationid` int NOT NULL,
+  `accommodationid` int NOT NULL,
   `locationid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -134,10 +161,10 @@ CREATE TABLE `users` (
 --
 
 --
--- Indexen voor tabel `accomodations`
+-- Indexen voor tabel `accommodations`
 --
-ALTER TABLE `accomodations`
-  ADD PRIMARY KEY (`accomodationid`);
+ALTER TABLE `accommodations`
+  ADD PRIMARY KEY (`accommodationid`);
 
 --
 -- Indexen voor tabel `flights`
@@ -172,7 +199,7 @@ ALTER TABLE `reviews`
 ALTER TABLE `trips`
   ADD PRIMARY KEY (`tripid`),
   ADD KEY `fk_Flight` (`flightid`),
-  ADD KEY `fk_users_accomodation` (`accomodationid`),
+  ADD KEY `fk_users_accommodation` (`accommodationid`),
   ADD KEY `fk_trips_locations` (`locationid`);
 
 --
@@ -186,10 +213,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT voor een tabel `accomodations`
+-- AUTO_INCREMENT voor een tabel `accommodations`
 --
-ALTER TABLE `accomodations`
-  MODIFY `accomodationid` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `accommodations`
+  MODIFY `accommodationid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT voor een tabel `flights`
+--
+ALTER TABLE `flights`
+  MODIFY `flightid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT voor een tabel `locations`
--- public/offers.php
+++ public/offers.php
@@ -1,3 +1,7 @@
+<?php
+include("dbcalls/conn.php");
+include("dbcalls/offers/read.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
