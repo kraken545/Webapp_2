8e56d0d622f72199266dbf06bf1380fab2307d28
Yonaki51 <yonivandon@hotmail.com>
2026-05-29 13:35:24 +0200

    changed read.php querry to feature joins

--- dbcalls/offers/read.php
+++ dbcalls/offers/read.php
@@ -1,6 +1,20 @@
 <?php
 
-$sql  = "SELECT * FROM trips";
+$sql = "SELECT t.tripid, t.price, t.startdate, t.duration,
+
+       a.name, a.type, a.image,
+
+       f.departure,
+
+       l.city, l.country
+
+FROM trips t
+
+LEFT JOIN accommodations a ON t.accommodationid = a.accommodationid
+
+LEFT JOIN flights f ON t.flightid = f.flightid
+
+INNER JOIN locations l ON t.locationid = l.locationid;";
 
 $stmt = $conn->prepare($sql);
 
