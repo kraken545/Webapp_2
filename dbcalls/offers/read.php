7bc61c4cd2be0edfcb573d5304725378d5ec2b0c
Yonaki51 <yonivandon@hotmail.com>
2026-06-04 16:35:34 +0200

    added a filter system to offers.php

--- dbcalls/offers/read.php
+++ dbcalls/offers/read.php
@@ -1,5 +1,31 @@
 <?php
 
+$people = 1;
+$departureDate = '';
+$days = '';
+$location = '';
+$accommodationType = '';
+
+if (isset($_GET['people'])) {
+    $people = $_GET['people'];
+}
+
+if (isset($_GET['departure-date'])) {
+    $departureDate = $_GET['departure-date'];
+}
+
+if (isset($_GET['days'])) {
+    $days = $_GET['days'];
+}
+
+if (isset($_GET['from']) && $_GET['from'] != '') {
+    $location = $_GET['from'];
+}
+
+if (isset($_GET['accommodation-type']) && $_GET['accommodation-type'] != '') {
+    $accommodationType = $_GET['accommodation-type'];
+}
+
 $sql = "SELECT 
 
     t.tripid, t.price, t.startdate, t.duration, t.description,
@@ -16,11 +42,50 @@ LEFT JOIN accommodations a ON t.accommodationid = a.accommodationid
 
 LEFT JOIN flights f ON t.flightid = f.flightid
 
-INNER JOIN locations l ON t.locationid = l.locationid;";
+INNER JOIN locations l ON t.locationid = l.locationid
+
+WHERE t.maxpersons >= :people";
+
+if ($departureDate !== '') {
+    $sql .= "\n\nAND DATE(t.startdate) = :departureDate";
+}
+
+if ($days !== '') {
+    $sql .= "\n\nAND t.duration = :days";
+}
+
+if ($location !== '') {
+    $sql .= "\n\nAND l.locationid = :location";
+}
+
+if ($accommodationType !== '') {
+    $sql .= "\n\nAND a.type = :accommodationType";
+}
 
 $stmt = $conn->prepare($sql);
+
+$stmt->bindParam(':people', $people);
+
+if ($departureDate !== '') {
+    $stmt->bindParam(':departureDate', $departureDate);
+}
+
+if ($days !== '') {
+    $stmt->bindParam(':days', $days);
+}
+
+if ($location !== '') {
+    $stmt->bindParam(':location', $location);
+}
+
+if ($accommodationType !== '') {
+    $stmt->bindParam(':accommodationType', $accommodationType);
+}
 
 $stmt->execute();
 
 $result = $stmt->fetchAll();
+
+
+
 
